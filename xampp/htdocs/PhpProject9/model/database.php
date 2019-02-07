dcba910f837cad38ce124dbe4ba66692af76ee29
Oisin Martin <Oisin Martin@10.102.11.164>
2019-02-07 14:34:49 +0000

    FIRST TECH COMMIT

---
+++ xampp/htdocs/PhpProject9/model/database.php
@@ -0,0 +1,13 @@
+<?php
+    $dsn = 'mysql:host=localhost;dbname=D00216083';
+    $username = 'root';
+    $password = '';
+
+    try {
+        $db = new PDO($dsn, $username, $password);
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        include('../errors/database_error.php');
+        exit();
+    }
+?>
